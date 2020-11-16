7cd2f2ea50a269882a6c8917039a7ced454d272b
LoremZyn14 <yasuaimo@gmail.com>
2020-11-16 13:42:33 +0800

    init account modules

---
+++ modules/accounts/models/accounts.js
@@ -0,0 +1,26 @@
+const { DataTypes, DATE } = require('sequelize')
+const db = require('./../../../services/db_connection')
+
+const account = db.sequelize.define("account", {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+    },
+    username: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+    password: {
+        allowNull: false,
+        type: DataTypes.STRING
+    },
+}, {
+    createdAt: true,
+    updatedAt: true,
+    deletedAt: true,
+    tableName: "accounts"
+})
+
+exports.account = account
