33faea93b2fd133f6e394ede0fdac4add4fc7c32
LoremZyn14 <yasuaimo@gmail.com>
2020-11-16 13:56:47 +0800

    add accounts migration

--- modules/accounts/migrations/20201116054139-create_accounts_table.js
+++ modules/accounts/migrations/20201116054139-create_accounts_table.js
@@ -1,21 +1,40 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+    up: async(queryInterface, Sequelize) => {
+        await queryInterface.createTable('accounts', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            username: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            password: {
+                allowNull: false,
+                type: Sequelize.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE
+            }
+        })
+    },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    down: async(queryInterface, Sequelize) => {
+
+        await queryInterface.dropTable('accounts');
+
+    }
 };
